75487300a1a2b82db50b03fe8766ac2dd7da6938
Pretty <mphefopretty1997@gmail.com>
2022-03-11 15:44:20 +0200

    added more tests

--- fruitsList.test.js
+++ fruitsList.test.js
@@ -6,6 +6,26 @@ describe('fruits_list function', function() {
             testingFruitsList.myFunction();
             assert.equal(testingFruitsList.myFunction())
         });
+        it('should allow the user to add a fruit with an emoji', function() {
+            let testingFruitsList = fruits_list();
+            testingFruitsList.myFunction();
+            assert.equal(testingFruitsList.myFunction())
+        });
+        it('should not allow the user to add a fruit without an emoji', function() {
+            let testingFruitsList = fruits_list();
+            testingFruitsList.myFunction();
+            assert.equal(testingFruitsList.myFunction())
+        });
+        it('should display an error message if the fruit name and emoji do not match', function() {
+            let testingFruitsList = fruits_list();
+            testingFruitsList.myFunction();
+            assert.equal(testingFruitsList.myFunction())
+        });
+        it('should diplay an error message saying please enter a fruit and emoji if the user clicks on the add button', function() {
+            let testingFruitsList = fruits_list();
+            testingFruitsList.myFunction();
+            assert.equal(testingFruitsList.myFunction())
+        });
         it('should display an error message when the user searches for a fruit that is not on the list', function() {
             let testingFruitsList = fruits_list();
             testingFruitsList.myFunction();
